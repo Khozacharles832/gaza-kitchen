68021bc15088eba1ee114077dbedf1d73fcda43a
Charlie <khozacharles832@gmail.com>
2025-08-21 01:08:32 +0200

    start with the auth logic

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,24 @@
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { images } from "@/constants";
 import { Slot } from "expo-router";
 import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from "react-native";
+//import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function AuthLayout() {
+export default function _Layout() {
     return (
-        <SafeAreaView>
-            <Text>Auth_Layout</Text>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                <View className="w-full relative" style={{ height: Dimensions.get('screen').height / 2.25}}>
+                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch"/>
+                    <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10" />
+                </View>
+                <CustomInput />
+                <CustomButton />
+            </ScrollView>
+                
             <Slot />
-        </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
---
+++ lib/appwrite.ts
@@ -0,0 +1,9 @@
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.rifumoApps.gazaKitchen",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_ID,
+    databaseId: "68a644ce002ace40306e",
+    userCollectionId: "68a645780008749b2dd0"
+    
+}
